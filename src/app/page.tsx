ed23f02237986fdaed12e10dc8818387f16084f8
Paulo Henrique <paulohenriqueribeiroalves@gmail.com>
2024-07-07 15:21:59 -0300

    Tiver que modificar o import do Team no componente Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/Theme-Toggle";
-import { Team } from "@/components/team";
+import { Team } from "../components/Team";
 
 const Page = () => {
     return (
