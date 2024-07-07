aef449bdd4101edd2d89820454ae21f72d7f337e
Paulo Henrique <paulohenriqueribeiroalves@gmail.com>
2024-07-07 15:20:27 -0300

    Adicionamos o DarckMode, depois vimos a aula de intelar o Darck no Vite e depois instalemos os componentes card, avatar e fizemos o componente Team

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/Theme-Toggle";
+import { Team } from "@/components/team";
 
 const Page = () => {
     return (
         <div className="min-h-screen flex justify-center items-center">
             <div className="max-w-2xl mx-auto">
                 <Alert>
+                    <ThemeToggle/>
                     <AlertTitle>Erro!</AlertTitle>
-                    <AlertDescription>Sua senha deu prolma!</AlertDescription>
+                    <Team/>
                 </Alert>
             </div>
         </div>
